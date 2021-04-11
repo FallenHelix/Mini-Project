25756e2c8483636053bf3d2eab807a36d346ed1d
shubh <shubhambhakuni26@gmail.com>
2021-04-11 14:59:24 +0530

    Training Image

---
+++ Files/TestCamera.py
@@ -0,0 +1,40 @@
+
+import cv2
+
+def test_camera():
+
+    # Load the cascade
+    cascadeClassifier = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+    print("Testing Camera , Press q to quit!") 
+    # To capture video from webcam.
+    # CAP_DSHOW => Remove Warning Sign 
+    cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)
+
+    while True:
+        # Read the frame
+        _, img = cap.read()
+
+        # Convert to grayscale
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+        # Detect the faces
+        faces = cascadeClassifier.detectMultiScale(gray, 1.3, 5, minSize=(30, 30),flags = cv2.CASCADE_SCALE_IMAGE)
+
+        # Draw the rectangle around each face
+        for (x, y, w, h) in faces:
+            cv2.rectangle(img, (x, y), (x + w, y + h), (255,0,0),2)
+
+
+        # Display
+        cv2.imshow('Testing Camera, Press q to quit', img)
+
+        # Stop if escape key is pressed
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            # cap.release()
+            cv2.destroyAllWindows()
+            break
+
+    # Release the VideoCapture object
+    print("Camera Realsed!!")
+    # cap.release()
+    # cv2.destroyAllWindows()
--- Files/main.py
+++ Files/main.py
@@ -1,55 +1,90 @@
 import os  # accessing the os functions
-import checkCamera as check_camera
-
+from TestCamera import test_camera
+from ImageCapture import capture_user_image ,takeImages
+import Train_Image
+import Recognize
 
 
 # creating the title bar function
 
-def title_bar():
-    os.system('cls')  # for windows
+def titleBar():
+    # os.system('cls')  # for windows
 
     # title of the program
 
     print("\t**********************************************")
-    print("\t***** Face Recognition Attendance System *****")
+    print("\t***** Python OpenCV Attendance System *****")
     print("\t**********************************************")
 
 
 # creating the user main menu function
+def main():
+    titleBar()
+    print("")
+    print("\t(1) Check Device's Camera")
+    print("\t(2) Capture Face Images")
+    print("\t(3) Train Images")
+    print("\t(4) Recognize Face and Mark Attendance")
+    print("\t(5) Send Mail")
+    print("\t(5) Exit")
 
-def mainMenu():
-    title_bar()
-    print()
-    print(10 * "*", "WELCOME MENU", 10 * "*")
-    print("[1] Check Camera")
-   
-    print("[2] Quit")
+    option = int(input("Select an Option: "))
 
-    while True:
-        try:
-            choice = int(input("Enter Choice: "))
-
-            if choice == 1:
-                checkCamera()
-                break
-            elif choice == 2:
-                print("Thank You")
-                break
-            else:
-                print("Invalid Choice. Enter 1-4")
-                mainMenu()
-        except ValueError:
-            print("Invalid Choice. Enter 1-4\n Try Again")
-    exit
+    if option == 1:
+        CheckCam()
+    elif option == 2:
+        CaptureImages()
+    elif option == 3:
+        TrainImages()
+    elif option == 4:
+        MarkAttendance()
+    elif option == 5:
+        # os.system("py automail.py")
+        main()
+    elif option == 6:
+        print("System Exited, Thank you!")
+        exit
+    else:
+        print("Invalid choice was selected. Select from 1-6")
+        main()
 
 
 # ---------------------------------------------------------
 # calling the camera test function from check camera.py file
 
-def checkCamera():
-    check_camera.camer()
+def CheckCam():
+    test_camera()
     key = input("Enter any key to return main menu")
-    mainMenu()
+    main()
 
 
-mainMenu()
+# --------------------------------------------------------------
+# calling the take image function form capture image.py file
+
+def CaptureImages():
+    # takeImages()
+    capture_user_image() 
+    key = input("Enter any key to return main menu")
+    main()
+
+
+# -----------------------------------------------------------------
+# calling the train images from train_images.py file
+
+def TrainImages():
+    Train_Image.TrainImages()
+    key = input("Enter any key to return main menu")
+    main()
+
+
+# --------------------------------------------------------------------
+# calling the recognize_attendance from recognize.py file
+
+def MarkAttendance():
+    Recognize.recognize_attendence()
+    key = input("Enter any key to return main menu")
+    main()
+
+
+# ---------------main driver ------------------
+main()
